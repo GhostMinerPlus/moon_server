aba31068d491bfd4197aabb30ecd45018d062e2b
ghostminer <1696415745@qq.com>
2024-02-18 17:05:15 +0800

    Clear all stars every minute

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,47 +1,77 @@
 //! Server that provides registration services.
 
-use std::{io, sync::Arc};
+use std::{future::Future, io, sync::Arc, time::Duration};
 
 use axum::{routing, Router};
+use tokio::time;
 
 use crate::state;
 
 mod service;
+
+async fn schedual<Task>(duration: Duration, task: Task)
+where
+    Task: Future<Output = io::Result<()>> + std::marker::Send + 'static,
+{
+    tokio::spawn(async move {
+        time::sleep(duration).await;
+        if let Err(e) = task.await {
+            log::error!("{e}");
+        }
+    });
+}
+
+async fn serve(server: &Server) -> io::Result<()> {
+    // build our application with a route
+    let app = Router::new()
+        .route(
+            &format!("/{}/report", server.name),
+            routing::post(service::http_report),
+        )
+        .route(
+            &format!("/{}/list", server.name),
+            routing::get(service::http_list),
+        )
+        .route(
+            &format!("/{}/get", server.name),
+            routing::get(service::http_get_uri),
+        )
+        .with_state(server.state.clone());
+
+    // run our app with hyper, listening globally on port 3000
+    let address = format!("{}:{}", server.ip, server.port);
+    log::info!("serving at {address}");
+    let listener = tokio::net::TcpListener::bind(address).await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
 
 // Public
 pub struct Server {
     ip: String,
     port: u16,
     name: String,
+    state: Arc<state::ServerState>,
 }
 
 impl Server {
     pub fn new(ip: String, port: u16, name: String) -> Self {
-        Self { ip, port, name }
+        Self {
+            ip,
+            port,
+            name,
+            state: Arc::new(state::ServerState::default()),
+        }
     }
 
     pub async fn run(self) -> io::Result<()> {
-        // build our application with a route
-        let app = Router::new()
-            .route(
-                &format!("/{}/report", self.name),
-                routing::post(service::http_report),
-            )
-            .route(
-                &format!("/{}/list", self.name),
-                routing::get(service::http_list),
-            )
-            .route(
-                &format!("/{}/get", self.name),
-                routing::get(service::http_get_address),
-            )
-            .with_state(Arc::new(state::ServerState::default()));
-
-        // run our app with hyper, listening globally on port 3000
-        let address = format!("{}:{}", self.ip, self.port);
-        log::info!("serving at {address}");
-        let listener = tokio::net::TcpListener::bind(address).await?;
-        axum::serve(listener, app).await?;
-        Ok(())
+        let state = self.state.clone();
+        schedual(Duration::from_secs(60), async move {
+            let mut client_v = state.client_v.lock().await;
+            client_v.clear();
+            Ok(())
+        })
+        .await;
+        serve(&self).await
     }
 }
--- src/server/service.rs
+++ src/server/service.rs
@@ -2,7 +2,11 @@
 
 use std::{collections::HashMap, sync::Arc};
 
-use axum::{extract::{Query, State}, http::StatusCode, Json};
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    Json,
+};
 use serde::{Deserialize, Serialize};
 
 use crate::state::ServerState;
@@ -11,7 +15,7 @@ use crate::state::ServerState;
 #[derive(Deserialize, Serialize)]
 pub struct ReportInfo {
     name: String,
-    address: String,
+    uri: String,
 }
 
 pub async fn http_report(
@@ -21,17 +25,17 @@ pub async fn http_report(
     log::info!("received from {}", report_info.name);
 
     let mut client_v = state.client_v.lock().await;
-    client_v.insert(report_info.name, report_info.address);
+    client_v.insert(report_info.name.clone(), report_info.uri);
     StatusCode::OK
 }
 
 pub async fn http_list(State(state): State<Arc<ServerState>>) -> (StatusCode, String) {
     let client_v = state.client_v.lock().await;
     let mut list = Vec::new();
-    for (name, address) in &*client_v {
+    for (name, uri) in &*client_v {
         list.push(ReportInfo {
             name: name.to_string(),
-            address: address.to_string(),
+            uri: uri.clone(),
         })
     }
     match serde_json::to_string(&list) {
@@ -40,14 +44,14 @@ pub async fn http_list(State(state): State<Arc<ServerState>>) -> (StatusCode, St
     }
 }
 
-pub async fn http_get_address(
+pub async fn http_get_uri(
     State(state): State<Arc<ServerState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> (StatusCode, String) {
     let client_v = state.client_v.lock().await;
     match params.get("name") {
         Some(name) => match client_v.get(name) {
-            Some(address) => (StatusCode::OK, address.clone()),
+            Some(uri) => (StatusCode::OK, uri.clone()),
             None => (StatusCode::INTERNAL_SERVER_ERROR, String::new()),
         },
         None => (StatusCode::INTERNAL_SERVER_ERROR, String::new()),
